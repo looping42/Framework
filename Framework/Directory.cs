779f333bee10e2a0bdb938d7e97ab6f4b67da7c0
looping42 <loopingr42@gmail.com>
2015-10-10 15:27:32 +0200

    ok

---
+++ Framework/Directory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.DirectoryMethod
+{
+    /****************************************************************
+    Fichier       :  DirectoryMethod
+    Auteur        :  ds
+    Mise à jour   :  10/10/2015
+    Fonction      :  création de répértoire
+    ******************************************************************/
+
+    public class DirectoryMethod
+    {
+        public static void CreateDirectory(string repertory)
+        {
+            //Si le répértoire n'existe pas
+            if (!Directory.Exists(repertory))
+            {
+                Directory.CreateDirectory(repertory);
+            }
+        }
+    }
+}
---
+++ Framework/Logger.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Logger
+{
+    /****************************************************************
+    Classe Logger :  Permet la logs des erreurs ,infos ,etc
+    Fichier       :  Logger
+    Auteur        :  ds
+    Mise à jour   :  10/10/2015
+    Fonction      :  log les erreurs
+    ******************************************************************/
+
+    public class Logger
+    {
+        public static void Error(string message, string module)
+        {
+            WriteEntry(message, "error", module);
+        }
+
+        public static void Error(Exception ex, string module)
+        {
+            WriteEntry(ex.Message, "error", module);
+        }
+
+        public static void Warning(string message, string module)
+        {
+            WriteEntry(message, "warning", module);
+        }
+
+        public static void Info(string message, string module)
+        {
+            WriteEntry(message, "info", module);
+        }
+
+        private static void WriteEntry(string message, string type, string module)
+        {
+            try
+            {
+                DirectoryMethod.DirectoryMethod.CreateDirectory(Properties.Settings.Default.Logs);
+                using (StreamWriter writer = new StreamWriter(Path.Combine(Properties.Settings.Default.Logs, DateExtension.DateExtension.DayToday()), true))
+                {
+                    writer.WriteLine(string.Format("{0},{1},{2},{3}",
+                                          DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"),
+                                          type,
+                                          module,
+                                          message));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
+        }
+    }
+}
