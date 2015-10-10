10826b677f6f831687db145439d8c77c4079732d
looping42 <loopingr42@gmail.com>
2015-10-10 15:22:01 +0200

    Travail sur le logger , directory et dateExtension

---
+++ Framework/DateExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.DateExtension
+{
+    /****************************************************************
+    Fichier       :  DateExtension
+    Auteur        :  ds
+    Mise à jour   :  10/10/2015
+    Fonction      :  appel des divers méthode comprenant des dates
+    ******************************************************************/
+
+    internal class DateExtension
+    {
+        public static string DayHourToday()
+        {
+            return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+
+        public static string DayToday()
+        {
+            return DateTime.Now.ToString("yyyy-MM-dd");
+        }
+    }
