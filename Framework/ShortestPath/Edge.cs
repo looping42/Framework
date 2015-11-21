eda21db353f6cb46361c127d253283a5087bd06f
looping42 <loopingr42@gmail.com>
2015-11-21 20:25:07 +0100

    Shortest Path Bellman Ford

---
+++ Framework/ShortestPath/Edge.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.ShortestPath
+{
+    public class Edge
+    {
+        public Node A { get; set; }
+        public Node B { get; set; }
+        public int Weight { get; set; }
+    }
+}
---
+++ Framework/ShortestPath/ShortestPath.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.ShortestPath
+{
+    public class BellmanFord
+    {
+        public List<Edge> edges { get; set; }
+
+        public List<Node> nodes { get; set; }
+
+        //Noeud de départ
+        public int posNodeStart { get; set; }
+
+        /// <summary>
+        /// Initialisation Bellman-Ford
+        /// </summary>
+        public void initialize()
+        {
+            foreach (Node n in nodes)
+            {
+                n.Value = int.MaxValue;
+                n.parent = null;
+            }
+            nodes[posNodeStart].Value = 0;
+        }
+
+        /// <summary>
+        ///retourne le poid entre 2 noeuds
+        /// </summary>
+        /// <param name="n1">Noeud 1</param>
+        /// <param name="n2">Noeud 2</param>
+        /// <returns>poids entre les 2 noeuds</returns>
+        public int getWeight(Node n1, Node n2)
+        {
+            int value = -1;
+            foreach (Edge edge in edges)
+            {
+                if (edge.A == n1 && edge.B == n2)
+                {
+                    value = edge.Weight;
+                }
+            }
+            return value;
+        }
+
+        /// <summary>
+        /// Relâche Les noeuds ,si la valeur du Noeud 2 est supérieur à la valeur du Noeud 1 + (valeur noeud1 +noued2 )
+        /// affecte cette valeur au noeud 2 et le noeud 1 devient son parent
+        /// </summary>
+        /// <param name="n1">Noeud 1</param>
+        /// <param name="n2">Noeud 2</param>
+        private void relax(Node n1, Node n2)
+        {
+            if (n2.Value > (n1.Value + getWeight(n1, n2)))
+            {
+                n2.Value = n1.Value + getWeight(n1, n2);
+                n2.parent = n1;
+            }
+        }
+
+        /// <summary>
+        /// Vérifie si on est dans un cycle négatif(probléme dans ce cas)
+        /// </summary>
+        /// <returns>booléen</returns>
+        public bool detectingNegativeCycles()
+        {
+            foreach (Edge edge in edges)
+            {
+                if (edge.B.Value > edge.A.Value + getWeight(edge.A, edge.B))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Alogorythme de Bellman-Ford
+        /// </summary>
+        public void bellmanFord()
+        {
+            initialize();
+
+            for (int i = 0; i <= nodes.Count - 2; i++)
+            {
+                foreach (Edge edge in edges)
+                {
+                    relax(edge.A, edge.B);
+                }
+            }
+            if (detectingNegativeCycles())
+            {
+                Log.Logger.Info("Cycle Négatif : non");
+            }
+            else
+            {
+                throw new Exception("nou sommes dans un Cycle Négatif");
+            }
+        }
+    }
+}
