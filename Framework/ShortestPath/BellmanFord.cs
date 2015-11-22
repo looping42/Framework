37847a31817454cba63f614ef3deee2bb3e1d413
looping42 <loopingr42@gmail.com>
2015-11-22 11:56:51 +0100

    ok

--- Framework/ShortestPath/BellmanFord.cs
+++ Framework/ShortestPath/BellmanFord.cs
@@ -11,61 +11,61 @@ namespace Framework.ShortestPath
     /// Trouve le plus court chemin pour chaque noeud en partant du noeud de départ
     /// la liste des noeud contenant le plus court chemin se trouve dans la List<Node> nodes en scannant les noeuds parents
     /// </summary>
-    public class BellmanFord
+    public class BellmanFord : UtilShortestPath
     {
-        public List<Edge> edges { get; set; }
+        //public List<Edge> edges { get; set; }
 
-        public List<Node> nodes { get; set; }
+        //public List<Node> nodes { get; set; }
 
-        //Noeud de départ
-        public int posNodeStart { get; set; }
+        ////Noeud de départ
+        //public int posNodeStart { get; set; }
 
-        /// <summary>
-        /// Initialisation Bellman-Ford
-        /// </summary>
-        public void initialize()
-        {
-            foreach (Node n in nodes)
-            {
-                n.Value = int.MaxValue;
-                n.parent = null;
-            }
-            nodes[posNodeStart].Value = 0;
-        }
+        ///// <summary>
+        ///// Initialisation Bellman-Ford
+        ///// </summary>
+        //public void initialize()
+        //{
+        //    foreach (Node n in nodes)
+        //    {
+        //        n.Value = int.MaxValue;
+        //        n.parent = null;
+        //    }
+        //    nodes[posNodeStart].Value = 0;
+        //}
 
-        /// <summary>
-        ///retourne le poid entre 2 noeuds
-        /// </summary>
-        /// <param name="n1">Noeud 1</param>
-        /// <param name="n2">Noeud 2</param>
-        /// <returns>poids entre les 2 noeuds</returns>
-        public int getWeight(Node n1, Node n2)
-        {
-            int value = -1;
-            foreach (Edge edge in edges)
-            {
-                if (edge.A == n1 && edge.B == n2)
-                {
-                    value = edge.Weight;
-                }
-            }
-            return value;
-        }
+        ///// <summary>
+        /////retourne le poid entre 2 noeuds
+        ///// </summary>
+        ///// <param name="n1">Noeud 1</param>
+        ///// <param name="n2">Noeud 2</param>
+        ///// <returns>poids entre les 2 noeuds</returns>
+        //public int getWeight(Node n1, Node n2)
+        //{
+        //    int value = -1;
+        //    foreach (Edge edge in edges)
+        //    {
+        //        if (edge.A == n1 && edge.B == n2)
+        //        {
+        //            value = edge.Weight;
+        //        }
+        //    }
+        //    return value;
+        //}
 
-        /// <summary>
-        /// Relâche Les noeuds ,si la valeur du Noeud 2 est supérieur à la valeur du Noeud 1 + (valeur noeud1 +noued2 )
-        /// affecte cette valeur au noeud 2 et le noeud 1 devient son parent
-        /// </summary>
-        /// <param name="n1">Noeud 1</param>
-        /// <param name="n2">Noeud 2</param>
-        private void relax(Node n1, Node n2)
-        {
-            if (n2.Value > (n1.Value + getWeight(n1, n2)))
-            {
-                n2.Value = n1.Value + getWeight(n1, n2);
-                n2.parent = n1;
-            }
-        }
+        ///// <summary>
+        ///// Relâche Les noeuds ,si la valeur du Noeud 2 est supérieur à la valeur du Noeud 1 + (valeur noeud1 +noued2 )
+        ///// affecte cette valeur au noeud 2 et le noeud 1 devient son parent
+        ///// </summary>
+        ///// <param name="n1">Noeud 1</param>
+        ///// <param name="n2">Noeud 2</param>
+        //private void relax(Node n1, Node n2)
+        //{
+        //    if (n2.Value > (n1.Value + getWeight(n1, n2)))
+        //    {
+        //        n2.Value = n1.Value + getWeight(n1, n2);
+        //        n2.parent = n1;
+        //    }
+        //}
 
         /// <summary>
         /// Vérifie si on est dans un cycle négatif(probléme dans ce cas)
---
+++ Framework/ShortestPath/UtilShortestPath.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.ShortestPath
+{
+    public class UtilShortestPath
+    {
+        public List<Edge> edges { get; set; }
+
+        public List<Node> nodes { get; set; }
+
+        //Noeud de départ
+        public int posNodeStart { get; set; }
+
+        /// <summary>
+        /// Initialisation Bellman-Ford
+        /// </summary>
+        public void initialize()
+        {
+            foreach (Node n in nodes)
+            {
+                n.Value = int.MaxValue;
+                n.parent = null;
+            }
+            nodes[posNodeStart].Value = 0;
+        }
+
+        /// <summary>
+        ///retourne le poid entre 2 noeuds
+        /// </summary>
+        /// <param name="n1">Noeud 1</param>
+        /// <param name="n2">Noeud 2</param>
+        /// <returns>poids entre les 2 noeuds</returns>
+        public int getWeight(Node n1, Node n2)
+        {
+            int value = -1;
+            foreach (Edge edge in edges)
+            {
+                if (edge.A == n1 && edge.B == n2)
+                {
+                    value = edge.Weight;
+                }
+            }
+            return value;
+        }
+
+        /// <summary>
+        /// Relâche Les noeuds ,si la valeur du Noeud 2 est supérieur à la valeur du Noeud 1 + (valeur noeud1 +noued2 )
+        /// affecte cette valeur au noeud 2 et le noeud 1 devient son parent
+        /// </summary>
+        /// <param name="n1">Noeud 1</param>
+        /// <param name="n2">Noeud 2</param>
+        public void relax(Node n1, Node n2)
+        {
+            if (n2.Value > (n1.Value + getWeight(n1, n2)))
+            {
+                n2.Value = n1.Value + getWeight(n1, n2);
+                n2.parent = n1;
+            }
+        }
+    }
+}
