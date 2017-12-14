b8fec3a50c8be551ed1bd94a28de94035aa99287
sylvain <sylvain@DELL-SYLVAIN>
2017-12-14 17:18:16 +0100

    Ajout recherche de toute les permutation d'une string

---
+++ Framework/BackTrace/PermutString.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.BackTrace
+{
+    public static class PermutString
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        private static void Swap(ref char a, ref char b)
+        {
+            char tmp;
+            tmp = a;
+            a = b;
+            b = tmp;
+        }
+
+        /// <summary>
+        /// Ecrit toute les permutations possible du tableau passer en entrée
+        /// </summary>
+        /// <param name="arry"></param>
+        /// <param name="i"></param>
+        /// <param name="n"></param>
+        public static void Permute(char[] arry, int i, int n)
+        {
+            int j;
+            if (i == n)
+                Console.WriteLine(arry);
+            else
+            {
+                for (j = i; j <= n; j++)
+                {
+                    Swap(ref arry[i], ref arry[j]);
+                    Permute(arry, i + 1, n);
+                    Swap(ref arry[i], ref arry[j]); //backtrack
+                }
+            }
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTestPermutString.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Framework.BackTrace;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTestPermutString
+    {
+        [TestMethod]
+        public void TestPermutString()
+        {
+            char[] str = "ABC".ToCharArray();
+            int n = str.Length;
+            PermutString.Permute(str, 0, n - 1);
+        }
+    }
+}
